be8b3bf78d92dbaebde0f99924cf5cea2cab93c2
shahwaz alam <shahwazait7303@gmail.com>
2022-09-06 04:29:10 +0530

    commit 2

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const todoRoutes = require('./routes/todo');
+
 
 const app = express();
-const PORT = 8000;
+dotenv.config();
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.json({'message': 'Server is running' }));
+app.get('/', (req, res) => res.json({ 'message': 'Server is running' }));
+app.get('/todo', todoRoutes);
 
-app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+
+mongoose.connect(DATABASE_URL, {useNewUrlparser: true })
+ .then(() => {
+    console.log('Database connection successful');
+    app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+ })
+ .catch((err) => {
+    console.log('Database connection failed');
+    console.log(err);
+})
---
+++ routes/todo.js
@@ -0,0 +1,72 @@
+const express = require('express').Router();
+const router = express.Router();
+const Todo = require('../models/todo');
+
+//List all todo
+router.get('/', (req, res) => {
+    Todo.find().exec((err, todos) => {
+        if (err) {
+            return res.json({ error: err });
+        }
+        return res.json({ data: todos });
+    });
+});
+
+
+
+// create a todo
+router.post('/create', (req, res) => {
+    const todo = Todo({
+        title: req.body.title,
+        content: req.body.content,
+    });
+    todo.save((err, todo) => {
+        if (err) {
+            return res.json({ error: err });
+        }
+        return res.json({ data: todo });
+    });
+});
+
+
+//edit a todo
+router.put('/:id', (req,res) => {
+    Todo.findById(req.params.id)
+     .exec((err, todo) => {
+        if (err) {
+            return res.json({ error: err });
+        }
+        todo.title = req.body.title ?? todo.title;
+        todo.content = req.body.content ?? todo.content;
+        todo.completed = req.body.completed ?? todo.completed;
+        todo.save((err, todo) =>{
+            if (err) {
+                return res.json({ error: err });
+            }
+
+            return res.json({ error: err });
+        })
+
+     });
+
+});
+
+
+//delete a todo 
+router.delete('/:id', (req, res) => {
+    Todo.remove({
+        _id: req.params.id
+    }).exec((err, result) => {
+        if (err) {
+            return res.json({ error: err });
+        }
+        if (result.deleteCount == 0) {
+            return res.json({ data: 'No Todo with given id'});
+        }
+        return res.json({ data: 'Deleted Successfully' });
+    })
+
+});
+
+module.exports = router;
+
